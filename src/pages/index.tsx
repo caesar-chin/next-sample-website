44417c67dd40bd6c3c34476bca6c0e9c69d378c6
caesar-chin <caesarc.1628@gmail.com>
2023-05-10 00:16:53 -0400

    set up prisma schema to trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,11 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 import { SignIn, SignInButton, UserButton, useUser } from "@clerk/nextjs";
-import { Sign } from "crypto";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -24,7 +23,11 @@ const Home: NextPage = () => {
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <UserButton />}
         </div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );

fb8b5a66cbad774955101b9b6f34f5621a9f0af7
caesar-chin <caesarc.1628@gmail.com>
2023-05-12 00:27:25 -0400

    render posts on single profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,7 +1,38 @@
 import { type GetStaticProps, type NextPage } from "next";
 import Head from "next/head";
 
+import { createServerSideHelpers } from "@trpc/react-query/server";
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
+import SuperJSON from "superjson";
+import PageLayout from "./components/layout";
+import Image from "next/image";
+import LoadingPage from "./components/loading";
+
 import { api } from "~/utils/api";
+import PostView from "./components/postview";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
+
+const ProfileFeed = (props: { userId: string }) => {
+  const { data, isLoading } = api.posts.getPostsByUserId.useQuery({
+    userId: props.userId,
+  });
+
+  if (isLoading) return <LoadingPage />;
+
+  if (!data || data.length === 0) return <div>User has not posted</div>;
+
+  return (
+    <div className="flex flex-col ">
+      {data.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
+  );
+};
 
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data } = api.profile.getUserByUsername.useQuery({
@@ -30,17 +61,11 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
           data.username ?? ""
         }`}</div>
         <div className="w-full border-b border-slate-400" />
+        <ProfileFeed userId={data.id} />
       </PageLayout>
     </>
   );
 };
-
-import { createServerSideHelpers } from "@trpc/react-query/server";
-import { appRouter } from "~/server/api/root";
-import { prisma } from "~/server/db";
-import SuperJSON from "superjson";
-import { PageLayout } from "./components/layout";
-import Image from "next/image";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const ssg = createServerSideHelpers({
